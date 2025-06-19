e6a0a5b0d6a3f2c6af238b19438218b349353d92
shruti <shrutimalodejain112004@gmail.com>
2025-06-19 22:20:16 +0530

    final change

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://shikshahub-community-frontend.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
